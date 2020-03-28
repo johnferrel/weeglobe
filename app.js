cbb526c026ded5ee2e8ce4979ea0a2e1e955c47e
John Ferrel <john.ferrel@savorycreative.com>
2020-03-28 00:23:36 -0700

    SASS compile on server restart

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
+  src: path.join(__dirname, 'public/stylesheets'),
+  dest: path.join(__dirname, 'public/stylesheets'),
+  indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
