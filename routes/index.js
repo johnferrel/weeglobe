af313842a15d96051d5215611db19922939e5d4f
John Ferrel <john.ferrel@savorycreative.com>
2020-03-28 20:49:14 -0700

    Latest updates images

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
                 category: 'Learn',
                 title: 'Captain Handwash and the Virus Villains!',
                 date: 'Mar 28, 2020',
-                image: 'news-1.png',
+                image: 'capt-handwash.png',
                 link: '#',
                 blurb: 'Halvah fruitcake biscuit oat cake I love sweet roll sweet biscuit lollipop. Jelly beans bear claw sweet roll I love chocolate gummies. Apple pie cheesecake cupcake carrot cake cookie bear claw jujubes. I love sugar plum sesame snaps.'
             },
@@ -18,7 +18,7 @@ router.get('/', function (req, res, next) {
                 category: 'Story',
                 title: 'The Girl Who Captured The Moon',
                 date: 'Mar 22, 2020',
-                image: 'news-2.png',
+                image: 'girl-captures-moon.png',
                 link: '#',
                 blurb: 'Gummi bears apple pie I love cake liquorice ice cream liquorice biscuit wafer. Chocolate bar biscuit pastry jelly-o danish jelly dessert. Danish sweet gummi bears chupa chups.'
             },
@@ -26,7 +26,7 @@ router.get('/', function (req, res, next) {
                 category: 'Activity',
                 title: 'Popsicle Pirate Ship',
                 date: 'Mar 19, 2020',
-                image: 'news-3.png',
+                image: 'popsicle-pirate-ship.png',
                 link: '#',
                 blurb: 'Gingerbread jelly beans I love chocolate cake carrot cake I love I love chupa chups. Cheesecake gummi bears cheesecake cake gummies candy wafer. Powder bear claw lollipop lollipop tiramisu sesame snaps gingerbread cake.'
             }
